4c16437c7d625d0f2abb0535ad1704b0720145e3
vermouth1995 <sixpenny1995@gmail.com>
2021-01-13 16:38:45 +0800

    🚧 update real array list

--- src/list/array-list.ts
+++ src/list/array-list.ts
@@ -1,30 +1,64 @@
 import { List } from '../list';
 
 export class ArrayList<E> implements List<E> {
-	constructor(isEqual: (left: E, right: E) => boolean) {
-		this.list = [];
+	constructor(isEqual: (left: E, right: E) => boolean, capacity?: number) {
+		if (capacity) {
+			this.capacity = capacity;
+		}
+		this.list = new Array<E>(this.capacity);
 		this.isEqual = isEqual;
+		this.length = 0;
 	}
 	private list: E[];
 	private isEqual: (left: E, right: E) => boolean;
+	private length: number; // 实际长度
+	private capacity: number = 10; // 数组容量
+	private static readonly FACTOR: number = 1.5; // 扩容系数
 
 	size(): number {
-		return this.list.length;
+		return this.length;
 	}
 	isEmpty(): boolean {
-		return this.list.length === 0;
+		return this.length === 0;
 	}
 	clear(): void {
-		this.list = [];
+		this.list = new Array<E>(this.capacity);
+		this.length = 0;
 		return;
 	}
+	private swell(newCapacity?: number) {
+		this.capacity = Math.ceil((newCapacity ? newCapacity : this.capacity) * ArrayList.FACTOR);
+		const newList = new Array<E>(this.capacity);
+		for (let i = 0; i < this.length; i++) {
+			newList[i] = this.list[i];
+		}
+		this.list = newList;
+	}
+	changeCapacity(newCapacity: number) {
+		if (newCapacity < this.length) {
+			throw new Error('capacity is too small');
+		}
+		this.capacity = Math.ceil(newCapacity);
+		const newList = new Array<E>(this.capacity);
+		for (let i = 0; i < this.length; i++) {
+			newList[i] = this.list[i];
+		}
+		this.list = newList;
+	}
 	add(ele: E): boolean {
-		this.list[this.size()] = ele;
+		if (this.length >= this.capacity) {
+			this.swell();
+		}
+		this.list[this.length] = ele;
+		this.length++;
 		return;
 	}
 	addAll(list: ArrayList<E>): boolean {
 		if (list.isEmpty()) {
 			return false;
+		}
+		if (this.length >= this.capacity + list.size()) {
+			this.swell(this.capacity + list.size());
 		}
 		list.iterate((ele) => {
 			this.add(ele);
@@ -33,7 +67,7 @@ export class ArrayList<E> implements List<E> {
 	}
 	contains(ele: E): boolean {
 		let flag = false;
-		for (let i = 0; i < this.size(); i++) {
+		for (let i = 0; i < this.length; i++) {
 			if (this.isEqual(ele, this.list[i])) {
 				flag = true;
 				break;
@@ -49,7 +83,7 @@ export class ArrayList<E> implements List<E> {
 		return flag;
 	}
 	iterate(on: (ele: E) => void): void {
-		for (let i = 0; i < this.size(); i++) {
+		for (let i = 0; i < this.length; i++) {
 			on(this.list[i]);
 		}
 		return;
@@ -60,14 +94,14 @@ export class ArrayList<E> implements List<E> {
 		}
 		while (this.indexOf(ele) !== -1) {
 			let pos = this.indexOf(ele);
-			if (pos === this.size() - 1) {
-				this.list.length -= 1;
+			if (pos === this.length - 1) {
+				this.length -= 1;
 				continue;
 			}
-			for (let i = pos; i < this.size() - 1; i++) {
-				this.set(i, this.get(i + 1));
+			for (let i = pos; i < this.length - 1; i++) {
+				this.set(i, this.list[i + 1]);
 			}
-			this.list.length -= 1;
+			this.length -= 1;
 		}
 		return true;
 	}
@@ -88,6 +122,7 @@ export class ArrayList<E> implements List<E> {
 		return flag;
 	}
 	retainAll(list: ArrayList<E>): boolean {
+		// TODO
 		let flag = false;
 		this.iterate((e) => {
 			if (!list.contains(e)) {
@@ -97,104 +132,104 @@ export class ArrayList<E> implements List<E> {
 		return flag;
 	}
 	set(index: number, ele: E): void {
-		if (index < 0 || index >= this.size()) {
+		if (index < 0 || index >= this.length) {
 			throw new Error('out of the boundary');
 		}
 		this.list[index] = ele;
 		return;
 	}
 	insert(index: number, ele: E): void {
-		if (index < 0 || index > this.size()) {
+		if (index < 0 || index > this.length) {
 			throw new Error('out of the boundary');
 		}
-		if (index === this.size()) {
+		if (index === this.length) {
 			this.add(ele);
 			return;
 		}
-		this.list.length += 1;
-		for (let i = this.size() - 1; i > index; i--) {
-			this.set(i, this.get(i - 1));
+		this.length += 1;
+		for (let i = this.length - 1; i > index; i--) {
+			this.set(i, this.list[i - 1]);
 		}
 		this.set(index, ele);
 		return;
 	}
 	insertAll(index: number, list: ArrayList<E>): boolean {
-		if (index < 0 || index > this.size()) {
+		if (index < 0 || index > this.length) {
 			throw new Error('out of the boundary');
 		}
-		if (list.size() === 0) {
+		if (list.length === 0) {
 			return false;
 		}
-		if (index === this.size()) {
+		if (index === this.length) {
 			this.addAll(list);
 			return true;
 		}
-		this.list.length += list.size();
-		for (let i = this.size() - 1; i > index; i--) {
-			this.set(i, this.get(i - list.size()));
+		this.length += list.length;
+		for (let i = this.length - 1; i > index; i--) {
+			this.set(i, this.list[i - list.length]);
 		}
-		for (let i = 0; i < list.size(); i++) {
+		for (let i = 0; i < list.length; i++) {
 			this.set(i + index, list.get(i));
 		}
 		return true;
 	}
 	get(index: number): E {
-		if (index < 0 || index >= this.size()) {
+		if (index < 0 || index >= this.length) {
 			throw new Error('out of the boundary');
 		}
 		return this.list[index];
 	}
 	indexOf(ele: E): number {
-		for (let i = 0; i < this.size(); i++) {
-			if (this.isEqual(this.get(i), ele)) {
+		for (let i = 0; i < this.length; i++) {
+			if (this.isEqual(this.list[i], ele)) {
 				return i;
 			}
 		}
 		return -1;
 	}
 	lastIndexOf(ele: E): number {
-		for (let i = this.size() - 1; i >= 0; i--) {
-			if (this.isEqual(this.get(i), ele)) {
+		for (let i = this.length - 1; i >= 0; i--) {
+			if (this.isEqual(this.list[i], ele)) {
 				return i;
 			}
 		}
 		return -1;
 	}
 	iterateFrom(index: number, on: (ele: E) => void): void {
-		if (index < 0 || index >= this.size()) {
+		if (index < 0 || index >= this.length) {
 			throw new Error('out of the boundary');
 		}
-		for (let i = index; i < this.size(); i++) {
+		for (let i = index; i < this.length; i++) {
 			on(this.list[i]);
 		}
 		return;
 	}
 	removeIndex(index: number): E {
-		if (index < 0 || index >= this.size()) {
+		if (index < 0 || index >= this.length) {
 			throw new Error('out of the boundary');
 		}
-		const ele = this.get(index);
-		if (index === this.size() - 1) {
-			this.list.length -= 1;
+		const ele = this.list[index];
+		if (index === this.length - 1) {
+			this.length -= 1;
 			return ele;
 		}
-		for (let i = index; i < this.size() - 1; i++) {
-			this.set(i, this.get(i + 1));
+		for (let i = index; i < this.length - 1; i++) {
+			this.set(i, this.list[i + 1]);
 		}
-		this.list.length -= 1;
+		this.length -= 1;
 		return ele;
 	}
 	replaceAll(operator: (ele: E) => E): void {
-		for (let i = 0; i < this.size(); i++) {
-			this.set(i, operator(this.get(i)));
+		for (let i = 0; i < this.length; i++) {
+			this.set(i, operator(this.list[i]));
 		}
 		return;
 	}
 	sort(compare: (left: E, right: E) => number): void {
-		for (let i = 1; i < this.size(); i++) {
-			const ele = this.get(i);
+		for (let i = 1; i < this.length; i++) {
+			const ele = this.list[i];
 			for (var j = i - 1; j >= 0; j--) {
-				const tmp = this.get(j);
+				const tmp = this.list[j];
 				const order = compare(tmp, ele);
 				if (order > 0) {
 					this.set(j + 1, tmp);
@@ -207,12 +242,12 @@ export class ArrayList<E> implements List<E> {
 		return;
 	}
 	subList(from: number, to: number): ArrayList<E> {
-		if (from > to || from < 0 || to >= this.size()) {
+		if (from > to || from < 0 || to >= this.length) {
 			throw new Error('out of the boundary');
 		}
 		const sub = new ArrayList(this.isEqual);
 		for (let i = from; i < to; i++) {
-			sub.add(this.get(i));
+			sub.add(this.list[i]);
 		}
 		return sub;
 	}
